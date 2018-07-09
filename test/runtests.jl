2e4c061282323c00584f92dab6e869ffb1bbcf9a
Robin Deits <robin.deits@gmail.com>
2018-07-09 17:10:30 -0400

    test with configurations within position limits

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,38 +12,41 @@ using RigidBodyDynamics
     state1 = MechanismState(mech1)
     state2 = MechanismState(mech2)
     matching_bodies =  ["pelvis", "r_foot_sole", "l_foot_sole", "r_hand_mount", "l_hand_mount"]
+    bounds = collect(Iterators.flatten(position_bounds(j) for j in joints(mech1)))
 
 
     @testset "basic interface" begin
         srand(1)
         for i in 1:10
             rand!(state1)
-            copy!(state2, Vector(state1) .+ 0.1 .* randn(22))
+            set_configuration!(state1,
+                clamp.(configuration(state1), bounds))
+            rand!(state2)
+            set_configuration!(state2,
+                clamp.(configuration(state2), bounds))
             for i in 1:10
                 IKMimic.ik_mimic!(state2, state1, matching_bodies)
             end
-            @test Vector(state2) ≈ Vector(state1) rtol=1e-3
+            @test Vector(state2) ≈ Vector(state1) rtol=5e-3
         end
     end
 
     @testset "in-place interface" begin
         srand(1)
-        successes = 0
         for i in 1:100
             rand!(state1)
-            copy!(state2, Vector(state1) .+ 0.1 .* randn(22))
+            set_configuration!(state1,
+                clamp.(configuration(state1), bounds))
+            rand!(state2)
+            set_configuration!(state2,
+                clamp.(configuration(state2), bounds))
             work = IKMimic.IKMimicWorkspace(state1, state2, matching_bodies)
             for i in 1:10
                 IKMimic.ik_mimic!(state2, state1, work)
             end
-            if isapprox(Vector(state2), Vector(state1), rtol=1e-3)
-                successes += 1
-            end
-            # @test Vector(state2) ≈ Vector(state1) rtol=1e-3
+            @test Vector(state2) ≈ Vector(state1) rtol=5e-3
             @test @allocated(IKMimic.ik_mimic!(state2, state1, work)) < 200
         end
-        @show successes
-        @test successes >= 77
     end
 
     # @testset "
